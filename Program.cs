62272c59be0297a0752625e2b256b3b2831a736e
Tony Redondo <tony.redondo@datadoghq.com>
2022-10-24 21:56:37 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var assemblyAttributes = assembly.GetCustomAttributes();
+foreach (var attr in assemblyAttributes)
+{
+    Console.WriteLine(attr.ToString());
+}
